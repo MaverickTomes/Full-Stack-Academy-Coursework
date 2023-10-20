bf9be3c5af06eaabbcbd283e2357e1bf2de870b5
Maverick Tomes <maverick.tomes@gmail.com>
2023-10-19 20:46:18 -0400

    updated solution for block 15 workshop

--- block15/froyoworkshop.js
+++ block15/froyoworkshop.js
@@ -10,7 +10,7 @@ let shopMenu = {
   ourPrices: ["small $2.50", "medium $3.50", "large $4.50"],
   ourDiscounts: "veterans 10% Off!",
 }
-
+console.table(shopMenu)
 //shopMenu.push("chocolate", "cookie dough"); <----this or  try below do not do anything to add flavors.
 //froyoFlavors.push("chocolate", "cookiedough"); <--not sure why this doesn't work to add flavors.
 
@@ -19,5 +19,17 @@ let shopMenu = {
                                               they ordered in the table or log.
 }
 */ 
-console.table(shopMenu)
+
+const order = {};
+for (let i = 0; i < froyoFlavors.length; i++) {
+    if(froyoFlavors[i] in order) {
+        order[froyoFlavors[i]] += 1;
+    }
+    else {
+        order[froyoFlavors[i]] = 1;
+    }
+}              
+return order;              
+//}  <---^^^doesn't work and the console.log below is grayed out. Not sure what I am doing wrong.
+console.table(froyoFlavors)
 console.log("These are your chosen flavors:" + " " +  froyoFlavors + " " + "Enjoy!");
