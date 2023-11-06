0d84727f6496c62aeea7404bf9f99bad75d4a057
Maverick Tomes <maverick.tomes@gmail.com>
2023-11-06 16:15:49 -0500

    saved app.jsx file only/rest corrupted and delted

---
+++ block25/workshop-app.jsx
@@ -0,0 +1,31 @@
+//<-------My react got corrupted when sending to the github this is the only filed I could save
+//this is also the only file I edited in the block 25 workshop so you can see below what I did ---->
+import { useState } from 'react'
+
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+
+};
+
+const App = () => {
+    const [selectedColor, setSelectedColor] = useState(" ")
+
+  return (
+    <div id="container">
+      <div id="navbar">
+        <div>Currently selected: </div>
+        <div className={selectedColor}>{selectedColor}</div>
+      </div>
+      <div id="colors-list"><Color color="yellow" setSelectedColor={setSelectedColor}/>
+                            <Color color="green" setSelectedColor={setSelectedColor}/>
+                            <Color color="blue" setSelectedColor={setSelectedColor}/>
+                            <Color color="red" setSelectedColor={setSelectedColor}/>
+      </div>
+    </div>
+  );
+};
+
+// Tons of errors in the console but it works somehow. I would not import properly so I 
+//just added it all in this app.jsx
+
+export default App;
